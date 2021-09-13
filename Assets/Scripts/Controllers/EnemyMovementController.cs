c061ea0de81c9f7bfc16fed0bf1d7e767f92b320
francorivaas <francoagustinrivas@hotmail.com>
2021-09-13 17:48:34 -0300

    now the spider recognise the player

--- Assets/Scripts/Controllers/EnemyMovementController.cs
+++ Assets/Scripts/Controllers/EnemyMovementController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class EnemyMovementController : EnemyController
 {
     private float distance = 2.0f;
+
     private Animator animator;
-    
+    private string moveAnim = "IsRunning";
+
     private void Start()
     {
         animator = GetComponent<Animator>();    
@@ -26,6 +28,11 @@ public class EnemyMovementController : EnemyController
             if (Vector3.Distance(transform.position, player.transform.position) > distance)
             {
                 transform.position = Vector3.MoveTowards(transform.position, player.transform.position, actorStats.Speed * Time.deltaTime);
+
+                if (animator != null)
+                {
+                    animator.SetBool(moveAnim, true);
+                }
             }
         }
     }
