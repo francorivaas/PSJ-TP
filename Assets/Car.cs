724cfe46950263e4919c04dc088e738dba40b8c7
francorivaas <francoagustinrivas@hotmail.com>
2021-10-02 16:16:24 -0300

    car with sound and particles

---
+++ Assets/Car.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem carSmoke;
+    private AudioSource horn;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horn = GetComponentInChildren<AudioSource>();
+
+        carSmoke.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            print("Debo encontrar ayuda, tal vez algunas herramientas sirvan. . .");
+            horn.Stop();
+        }
+    }
+}
